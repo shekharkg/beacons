0933486a1db8633d66ca3de185418c6b3a7b9290
raju <raju@gmail.com>
2017-05-18 01:15:54 +0530

    Yo

--- app/src/main/java/com/shekharkg/beacons/MainActivity.java
+++ app/src/main/java/com/shekharkg/beacons/MainActivity.java
@@ -2,12 +2,13 @@ package com.shekharkg.beacons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    Toast.makeText(getApplicationContext(),"Main Activity ",Toast.LENGTH_SHORT).show();
   }
 }
